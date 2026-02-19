102b780e3ead4549ac89ea83b66a77e1686c03c7
mineDiamond <mine_yaoym@outlook.com>
2026-02-19 23:00:37 +0800

    feat: 为Tag添加更多注释

--- src/main/java/tech/minediamond/micanbt/tag/Tag.java
+++ src/main/java/tech/minediamond/micanbt/tag/Tag.java
@@ -11,6 +11,8 @@ import tech.minediamond.micanbt.path.NBTPathParseException;
  * <p>
  * NBT tags are used to store structured data in a tree-like format. Each tag
  * consists of an optional name and a payload defined by its specific implementation.
+ *
+ * @see <a href="https://minecraft.wiki/w/NBT_format#TAG_definition">Minecraft Wiki: NBT format</a>
  */
 public abstract class Tag {
     private final String name;
@@ -89,6 +91,33 @@ public abstract class Tag {
         return at(NBTPath.fromParts(parts));
     }
 
+    /**
+     * Get the tag type ID defined according to the NBT specification.
+     * <table>
+     *     <caption>id table</caption>
+     *   <thead>
+     *     <tr><th>Tag type</th><th>ID</th></tr>
+     *   </thead>
+     *   <tbody>
+     *     <tr><td>TAG_Byte</td><td>{@value ByteTag#ID}</td></tr>
+     *     <tr><td>TAG_Short</td><td>{@value ShortTag#ID}</td></tr>
+     *     <tr><td>TAG_Int</td><td>{@value IntTag#ID}</td></tr>
+     *     <tr><td>TAG_Long</td><td>{@value LongTag#ID}</td></tr>
+     *     <tr><td>TAG_Float</td><td>{@value FloatTag#ID}</td></tr>
+     *     <tr><td>TAG_Double</td><td>{@value DoubleTag#ID}</td></tr>
+     *     <tr><td>TAG_Byte_Array</td><td>{@value ByteArrayTag#ID}</td></tr>
+     *     <tr><td>TAG_String</td><td>{@value StringTag#ID}</td></tr>
+     *     <tr><td>TAG_List</td><td>{@value ListTag#ID}</td></tr>
+     *     <tr><td>TAG_Compound</td><td>{@value CompoundTag#ID}</td></tr>
+     *     <tr><td>TAG_Int_Array</td><td>{@value IntArrayTag#ID}</td></tr>
+     *     <tr><td>TAG_Long_Array</td><td>{@value LongArrayTag#ID}</td></tr>
+     *   </tbody>
+     * </table>
+     *
+     * @return ID of the tag type.
+     *
+     * @see <a href="https://minecraft.wiki/w/NBT_format#TAG_definition">Minecraft Wiki: NBT format</a>
+     */
     public abstract int getTagId();
 
     /**
@@ -126,7 +155,7 @@ public abstract class Tag {
      *
      * @param stringifyRootTagName does snbt include tag names
      * @param snbtStyle            The format of SNBT
-     * @return the SNBT present of this tag.
+     * @return the SNBT representation of this tag.
      * @see SNBTStyle
      */
     public String toString(boolean stringifyRootTagName, SNBTStyle snbtStyle) {
